7927da1e38cdc3b6674935c3ea691ccc8a6f537b
profcrabbe <crabbe@usna.edu>
2015-02-16 16:26:49 -0500

    Flush-out of the makeCacheMatrix skeleton.  Appears to be complete.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+################################################################################
+## makeCacheMatrix(x) - creates a Matrix list-object that contains a closure
+##  that caches in inverse of the matrix.
+## ARGS: x is the initial matrix.  It must be invertable.  If not, attempting
+##  to invert will create a crack in time and space.
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL # The inverse.  Initially empty
+  set <- function(y) { # set is a closure, capturing x, which holds the matrix
+    x <<- y
+    i <<- NULL  #set changes x, so any cached value of inverse must be dumped
+  }
+  get <- function() x
+  #getinverse and setinverse should only be used in cacheSolve.  Would be made private in
+  #a proper OO systetm
+  setinverse <- function(inv) i <<- inv
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +24,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)){
+    return(i)
+  }
+  theMatrix <- x$get()
+  i <- solve(theMatrix)
+  x$setinverse(i)
+  i
 }
